a3c62295f8b9a135efe7aaf57d8a3c4722e31721
douguizilla <douglasgommez@gmail.com>
2021-07-09 17:52:34 -0300

    setting BookDetailsActivity to show up intent infos on screen

--- app/src/main/java/com/odougle/nytbooksapi/presentation/details/BookDetailsActivity.kt
+++ app/src/main/java/com/odougle/nytbooksapi/presentation/details/BookDetailsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import com.odougle.nytbooksapi.R
 import com.odougle.nytbooksapi.presentation.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_book_details.*
 import kotlinx.android.synthetic.main.include_toolbar.*
 
 class BookDetailsActivity : BaseActivity() {
@@ -14,6 +15,13 @@ class BookDetailsActivity : BaseActivity() {
 
         //set toolbar
         setupToolbar(toolbarmain,R.string.books_details_title)
+
+        //set views
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val description = intent.getStringExtra(EXTRA_DESCRIPTION)
+
+        book_detail_title.text = title
+        book_detail_description.text = description
     }
 
     companion object{
