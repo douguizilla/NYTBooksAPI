0698a3416ddb996069e2d50729fc4733ac1a09c4
douguizilla <douglasgommez@gmail.com>
2021-07-09 14:29:43 -0300

    refactor BookActivity.kt to BooksActivity.kt, set toolbar in BooksActivity.kt and create books_title in strings.xml

--- app/src/main/java/com/odougle/nytbooksapi/books/BooksActivity.kt
+++ app/src/main/java/com/odougle/nytbooksapi/books/BooksActivity.kt
@@ -3,12 +3,15 @@ package com.odougle.nytbooksapi.books
 import android.os.Bundle
 import com.odougle.nytbooksapi.R
 import com.odougle.nytbooksapi.base.BaseActivity
+import kotlinx.android.synthetic.main.include_toolbar.*
 
-class BookActivity : BaseActivity() {
+class BooksActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book)
+        setContentView(R.layout.activity_books)
 
+        //set toolbar
+        setupToolbar(toolbarmain, R.string.books_title)
 
 
     }
