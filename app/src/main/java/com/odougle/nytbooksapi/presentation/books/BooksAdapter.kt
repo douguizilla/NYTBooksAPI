68b1fdd621c8c2e10965040809775089b0789d94
douguizilla <douglasgommez@gmail.com>
2021-07-09 17:35:48 -0300

    Add clickListener as a parameter on Adapter to open details activity when click on recycler view item

--- app/src/main/java/com/odougle/nytbooksapi/presentation/books/BooksAdapter.kt
+++ app/src/main/java/com/odougle/nytbooksapi/presentation/books/BooksAdapter.kt
@@ -9,12 +9,13 @@ import com.odougle.nytbooksapi.data.model.Book
 import kotlinx.android.synthetic.main.item_book.view.*
 
 class BooksAdapter(
-    val books: List<Book>
+    val books: List<Book>,
+    val onItemClickListener: ((book: Book) -> Unit)
 ): RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
-        return BooksViewHolder(itemView)
+        return BooksViewHolder(itemView, onItemClickListener)
     }
 
     override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
@@ -23,13 +24,20 @@ class BooksAdapter(
 
     override fun getItemCount() = books.count()
 
-    class BooksViewHolder(view: View): RecyclerView.ViewHolder(view){
+    class BooksViewHolder(
+        view: View,
+        private val onItemClickListener: ((book: Book) -> Unit)
+    ): RecyclerView.ViewHolder(view){
         private val title = view.textTitle
         private val author = view.textAuthor
 
         fun bindView(book: Book){
             title.text = book.title
             author.text = book.author
+
+            itemView.setOnClickListener {
+                onItemClickListener.invoke(book)
+            }
         }
     }
 }
