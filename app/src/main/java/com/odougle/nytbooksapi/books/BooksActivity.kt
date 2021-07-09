37f2d1311d3bfc335de34e37e565c7db508c3f08
douguizilla <douglasgommez@gmail.com>
2021-07-09 15:30:52 -0300

    Problem: nothing is showing up when try to compile. I think is 'cause I`ve not changed AndroidManifest about the current Main Activity.

--- app/src/main/java/com/odougle/nytbooksapi/books/BooksActivity.kt
+++ app/src/main/java/com/odougle/nytbooksapi/books/BooksActivity.kt
@@ -1,8 +1,12 @@
 package com.odougle.nytbooksapi.books
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.odougle.nytbooksapi.R
 import com.odougle.nytbooksapi.base.BaseActivity
+import com.odougle.nytbooksapi.data.Book
+import kotlinx.android.synthetic.main.activity_books.*
 import kotlinx.android.synthetic.main.include_toolbar.*
 
 class BooksActivity : BaseActivity() {
@@ -13,6 +17,34 @@ class BooksActivity : BaseActivity() {
         //set toolbar
         setupToolbar(toolbarmain, R.string.books_title)
 
+        //set recyclerview
+        with(recycler_books_list){
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = BooksAdapter(getFakeListBooks())
+        }
 
+
+    }
+
+    fun getFakeListBooks(): List<Book>{
+        return listOf(
+            Book(
+                "Title 1",
+                "Author 1"
+            ),
+            Book(
+                "Title 2",
+                "Author 2"
+            ),
+            Book(
+                "Title 3",
+                "Author 3"
+            ),
+            Book(
+                "Title 4",
+                "Author 4"
+            )
+            )
     }
 }
