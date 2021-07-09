16941dfae659302812d3a97aeabcd42d810372dd
douguizilla <douglasgommez@gmail.com>
2021-07-09 16:21:27 -0300

    add BookBodyResponse.kt call as return for getBooks function

--- app/src/main/java/com/odougle/nytbooksapi/data/NYTServices.kt
+++ app/src/main/java/com/odougle/nytbooksapi/data/NYTServices.kt
@@ -1,5 +1,7 @@
 package com.odougle.nytbooksapi.data
 
+import com.odougle.nytbooksapi.data.response.BookBodyResponse
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,5 +10,5 @@ interface NYTServices {
     fun getBooks(
         @Query("api-key") apiKey: String = "jAge5QLXBC5YSJ4hCEKF9vrRfakq4cJ7",
         @Query("list") list: String = "hadcorver-fiction"
-    )
+    ): Call<BookBodyResponse>
 }
