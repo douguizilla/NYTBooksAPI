696e6a815d3dba9092264aec77e6cffedc7d8dee
douguizilla <douglasgommez@gmail.com>
2021-07-09 15:01:00 -0300

    create fist steps of BooksAdapter.kt

---
+++ app/src/main/java/com/odougle/nytbooksapi/books/BooksAdapter.kt
@@ -0,0 +1,25 @@
+package com.odougle.nytbooksapi.books
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BooksAdapter(
+): RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class BooksViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+}
